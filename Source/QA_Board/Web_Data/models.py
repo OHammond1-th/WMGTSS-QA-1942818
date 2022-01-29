618520c56fbc4f8fe9a4619fc7647c7895fd8221
Oliver Hammond <OHammond1@hotmail.com>
2022-01-29 15:59:06 +0000

    Added comment dataclass

--- Source/QA_Board/Web_Data/models.py
+++ Source/QA_Board/Web_Data/models.py
@@ -3,11 +3,21 @@ from dataclasses import dataclass
 import datetime
 import time
 
+
+@dataclass
+class Comment:
+    id: int
+    description: str
+    date_created: datetime.datetime
+
+
 @dataclass
 class Question:
 
+    id: int
     title: str
     description: str
     answer: str
     date_created: datetime.datetime
     published: bool
+    comments: list
