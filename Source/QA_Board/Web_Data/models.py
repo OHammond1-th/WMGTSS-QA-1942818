09a6e5bd515ce9b7d124ce06554de9dad9f25899
Oliver Hammond <OHammond1@hotmail.com>
2022-01-29 16:28:29 +0000

    Added update function to model

--- Source/QA_Board/Web_Data/models.py
+++ Source/QA_Board/Web_Data/models.py
@@ -9,6 +9,7 @@ class Comment:
     id: int
     description: str
     date_created: datetime.datetime
+    parent: int = None
 
 
 @dataclass
@@ -20,7 +21,7 @@ class Question:
     answer: str
     date_created: datetime.datetime
     published: bool
-    comments: list
+    comments: list = None
 
 
 class Model:
@@ -28,12 +29,26 @@ class Model:
     def __init__(self, user_id, role):
         self.user_id = user_id
         self.role = role
+        self.questions = []
         self.update()
 
     def update(self):
 
-        self.user_questions = [Question() for question in wmgtss_qa.get_posts_by_user(self.user_id)]
+        self.questions = [
+            Question(
+                question['post_id'], question['post_title'], question['post_description'], question['post_answer'],
+                question['post_created'], question['post_published']
+            )
+            for question
+            in wmgtss_qa.get_posts_by_user(self.user_id)
+        ]
 
-        for question in self.user_questions:
-            question.comments = [Comment() for comment in wmgtss_qa.get_post_comments(question.id)]
+        for question in self.questions:
+            question.comments = [
+                Comment(
+                    comment['comment_id'], comment['comment_description'], comment['comment_created'],
+                    comment['parent_id'])
+                for comment
+                in wmgtss_qa.get_post_comments(question.id)
+            ]
 
